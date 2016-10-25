2cb951c8099df48dd3835275636e27aae56a8795
江文得 <82697451@qq.com>
2016-10-25 15:30:08 +0800

    update

---
+++ Application/Admin/Controller/MenuController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+class MenuController extends Controller {
+    public function index(){
+        $this->display();
+    }
+
+    public function add(){
+        $this->display();
+    }
+}
